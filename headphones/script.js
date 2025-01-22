e83cfb035c517f0c3076e1339ce2e0f58f2dc35f
pc@DESKTOP-SDPG257 <getalemayhu@gmail.com>
2025-01-22 12:52:36 +0300

    fix: correct spelling of 'hamburger' and update related styles and scripts

--- headphones/script.js
+++ headphones/script.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const humburger = document.getElementById("humburger");
-    const navlinks = document.getElementById("nav-links");
- 
-    humburger.addEventListener("click", function() {
-        humburger.classList.toggle("active");
-        navlinks.classList.toggle("active");
-    });
- 
+document.addEventListener("DOMContentLoaded", function (){
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
 
+    hamburger.addEventListener("click", function(){ 
+            navLinks.classList.toggle("active");
+            hamburger.classList.toggle("active");
+            
+        }); 
 });
